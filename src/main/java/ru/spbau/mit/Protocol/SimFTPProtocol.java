e956b68be3303063840cd0ff3619d901ac69384c
Shamil Garifullin <shamil.garifullin@gmail.com>
2016-10-13 09:40:53 +0300

    started work2

--- src/main/java/ru/spbau/mit/Protocol/SimFTPProtocol.java
+++ src/main/java/ru/spbau/mit/Protocol/SimFTPProtocol.java
@@ -2,11 +2,13 @@ package ru.spbau.mit.Protocol;
 
 import ru.spbau.mit.Protocol.Responses.Response;
 
+import java.io.IOException;
+
 // Client and server parts?
 public interface SimFTPProtocol {
     byte[] formListRequest(String path);
     byte[] formGetRequest(String path);
     Response readResponse(byte[] contents);
 
-    Response formResponse(byte[] request);
+    Response formResponse(byte[] request) throws IOException;
 }
--- src/main/java/ru/spbau/mit/Protocol/SimFTPProtocolImpl.java
+++ src/main/java/ru/spbau/mit/Protocol/SimFTPProtocolImpl.java
@@ -1,11 +1,16 @@
 package ru.spbau.mit.Protocol;
 
+import org.apache.commons.io.FileUtils;
 import ru.spbau.mit.Protocol.Requests.GetRequest;
 import ru.spbau.mit.Protocol.Requests.ListRequest;
 import ru.spbau.mit.Protocol.Requests.Request;
+import ru.spbau.mit.Protocol.Responses.GetResponse;
+import ru.spbau.mit.Protocol.Responses.ListResponse;
 import ru.spbau.mit.Protocol.Responses.Response;
 
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class SimFTPProtocolImpl implements SimFTPProtocol {
 
@@ -27,26 +32,40 @@ public class SimFTPProtocolImpl implements SimFTPProtocol {
     }
 
     private Request parseRequest(byte[] input) {
+        ByteBuffer bb = ByteBuffer.wrap(input);
+        bb.getChar();
         return null;
     }
 
     @Override
-    public Response formResponse(byte[] request) {
+    public Response formResponse(byte[] request) throws IOException {
         Request r = parseRequest(request);
-        switch (r.getName()){
+        switch (r.getName()) {
             case "get":
-                return formGetResponse(r.getArgs());
+                return formGetResponse(r.getArgs().get(0));
             case "list":
-                return formListResponse(r.getArgs());
+                return formListResponse(r.getArgs().get(0));
         }
         throw new IllegalStateException("Can't be here");
     }
 
-    private Response formListResponse(List<String> args) {
-        return null;
+    private Response formListResponse(String path) {
+        File f = new File(path);
+        if (!f.exists()) {
+            return new ListResponse(0);
+        }
+        if (f.isFile()) {
+            return new ListResponse(1, path, false);
+        }
+        return new ListResponse(f.listFiles().length, path, true);
     }
 
-    private Response formGetResponse(List<String> args) {
-        return null;
+    private Response formGetResponse(String path) throws IOException {
+        File f = new File(path);
+        if (!f.exists()){
+            return new GetResponse(0, null);
+        }
+        byte[] bytes = FileUtils.readFileToByteArray(f);
+        return new GetResponse(bytes.length, bytes);
     }
 }
