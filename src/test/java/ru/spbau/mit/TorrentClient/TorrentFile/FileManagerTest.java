6a7455a647a5d8a6f787c9bbc4f28dcb3ba72041
Shamil Garifullin <shamil.garifullin@gmail.com>
2016-11-07 23:48:26 +0300

    Added filemanager tests

--- src/test/java/ru/spbau/mit/TorrentClient/TorrentFile/FileManagerTest.java
+++ src/test/java/ru/spbau/mit/TorrentClient/TorrentFile/FileManagerTest.java
@@ -7,12 +7,16 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import ru.spbau.mit.Protocol.Client.ClientProtocol;
 import ru.spbau.mit.Protocol.Client.ClientProtocolImpl;
+import ru.spbau.mit.Protocol.RemoteFile;
 
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.Socket;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -30,43 +34,52 @@ public class FileManagerTest {
     private File f;
     private File f2;
     private File dir;
-
-
-    private ByteArrayOutputStream outContent;
-    private DataInputStream inContent;
-    private Socket sock;
-    private final ClientProtocol prot = new ClientProtocolImpl();
+    private FileManager fm;
 
     @Before
     public void setUpStreams() throws IOException {
-        outContent = new ByteArrayOutputStream();
         folder.newFolder(DIR.split("/")[0]);
         folder.newFolder(DIR);
         f = folder.newFile(DIR + "/" + F1);
         dir = folder.newFolder(F2DIR);
         f2 = folder.newFile(F2DIR + "/" + F2);
         FileUtils.writeByteArrayToFile(f2, F2DIR.getBytes());
-        System.setProperty("user.dir", folder.getRoot().getAbsolutePath());
+        fm = new FileManager(folder.getRoot().getAbsoluteFile());
     }
 
     @Test
-    public void getFileIds() throws Exception {
-
-    }
-
-    @Test
-    public void getTorrentFile() throws Exception {
-
+    public void addTorrentFile() throws Exception {
+        fm.addTorrentFile(f2.getAbsoluteFile(), new RemoteFile(0, f2.getName(), f2.length()));
+        TorrentFileLocal file = fm.getTorrentFile(0);
+        assertEquals(f2, file.getFile());
     }
 
     @Test
     public void createTorrentFile() throws Exception {
+        fm.createTorrentFile(f2.getParentFile(), new RemoteFile(0, f2.getName() + "23", f2.length()));
+        TorrentFileLocal file = fm.getTorrentFile(0);
+        assertEquals(f2.length(), file.getFile().length());
+    }
 
+    @Test(expected = FileAlreadyExistsException.class)
+    public void createExistingTorrentFile() throws Exception {
+        fm.createTorrentFile(f2.getParentFile(), new RemoteFile(0, f2.getName(), f2.length()));
+    }
+
+    @Test(expected = FileAlreadyExistsException.class)
+    public void addSameID() throws Exception {
+        fm.createTorrentFile(f2.getParentFile(), new RemoteFile(0, f2.getName() + "23", f2.length()));
+        fm.addTorrentFile(f2.getAbsoluteFile(), new RemoteFile(0, f2.getName(), f2.length()));
     }
 
     @Test
-    public void saveToDisk() throws Exception {
-
+    public void saveState() throws IOException {
+        fm.createTorrentFile(f2.getParentFile(), new RemoteFile(0, f2.getName() + "23", f2.length()));
+        fm.addTorrentFile(f2.getAbsoluteFile(), new RemoteFile(1, f2.getName(), f2.length()));
+        fm.saveToDisk();
+        fm = new FileManager(folder.getRoot().getAbsoluteFile());
+        assertEquals(Arrays.asList(0 , 1), fm.getFileIds());
+        assertEquals(f2, fm.getTorrentFile(1).getFile());
     }
 
 }
